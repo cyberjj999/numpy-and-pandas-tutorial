fefea0fbf09e79923fda2ee439efe360dcc90d8d
Lye Jia Jun <lyejiajun99@gmail.com>
2024-01-03 20:18:10 +0800

    feat: add Data36 pandas tutorial dataset and notebook

---
+++ Data36 Pandas Tutorial/test.py
@@ -0,0 +1,14 @@
+import os
+
+# current_directory = os.getcwd()
+# print(current_directory)
+# if not os.path.exists('pandas_tutorial_read'):
+#     print("it exist")
+# else:
+#     print('lol')
+
+
+
+    
+x = os.path.exists("zoao.csv")
+print(x)
